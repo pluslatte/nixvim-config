4f5758783c7f43009fcf068a0427047cdd699408
pluslatte <debrisfield0201@gmail.com>
2024-12-11 18:58:20 +0900

    Leader then e to open neo-tree

--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -2,6 +2,16 @@
   plugins = {
     neo-tree = {
       enable = true;
+
+      keymaps = [
+        {
+          key = "<Leader>e";
+          action = "<cmd>Neotree toggle<cr>";
+          options = {
+            desc = "Toggle NeoTree";
+          };
+        }
+      ];
     };
     web-devicons.enable = true;
   };
