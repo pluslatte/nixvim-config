c33256360efe170062588352447bbb35eccb984d
pluslatte <debrisfield0201@gmail.com>
2024-12-11 21:26:29 +0900

    buffer keymap

--- config/plugins/bufferline.nix
+++ config/plugins/bufferline.nix
@@ -3,4 +3,42 @@
     bufferline.enable = true;
     web-devicons.enable = true;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<Leader>bc";
+      action = "<cmd>bd<CR>";
+      options = {
+        desc = "Close this buffer";
+        group = "Buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<Leader>bh";
+      action = "<cmd>BufferLineCyclePrev";
+      options = {
+        desc = "Select <- Buffer";
+        group = "Buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<Leader>bl";
+      action = "<cmd>BufferLineCycleNext";
+      options = {
+        desc = "Select -> Buffer";
+        group = "Buffer";
+      };
+    }
+    {
+      mode = "n";
+      key = "<Leader>ba";
+      action = "<cmd>BufferLineCloseOthers";
+      options = {
+        desc = "Close all buffer except this";
+      };
+    }
+  ];
 }
