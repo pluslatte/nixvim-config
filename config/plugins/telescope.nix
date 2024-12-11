472aa42667488c578835d8f6998e3bbf9da55789
pluslatte <debrisfield0201@gmail.com>
2024-12-11 20:24:19 +0900

    telescope config change

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -7,88 +7,134 @@
         fzf-native.enable = true;
         ui-select.enable = true;
       };
+      keymaps = {
+        "<leader>sh" = {
+          mode = "n";
+          action = "help_tags";
+          options = {
+            desc = "[S]earch [H]elp";
+          };
+        };
+        "<leader>sk" = {
+          mode = "n";
+          action = "keymaps";
+          options = {
+            desc = "[S]earch [K]eymaps";
+          };
+        };
+        "<leader>sf" = {
+          mode = "n";
+          action = "find_files";
+          options = {
+            desc = "[S]earch [F]iles";
+          };
+        };
+        "<leader>ss" = {
+          mode = "n";
+          action = "builtin";
+          options = {
+            desc = "[S]earch [S]elect Telescope";
+          };
+        };
+        "<leader>sw" = {
+          mode = "n";
+          action = "grep_string";
+          options = {
+            desc = "[S]earch current [W]ord";
+          };
+        };
+        "<leader>sg" = {
+          mode = "n";
+          action = "live_grep";
+          options = {
+            desc = "[S]earch by [G]rep";
+          };
+        };
+        "<leader>sd" = {
+          mode = "n";
+          action = "diagnostics";
+          options = {
+            desc = "[S]earch [D]iagnostics";
+          };
+        };
+        "<leader>sr" = {
+          mode = "n";
+          action = "resume";
+          options = {
+            desc = "[S]earch [R]esume";
+          };
+        };
+        "<leader>s" = {
+          mode = "n";
+          action = "oldfiles";
+          options = {
+            desc = "[S]earch Recent Files ('.' for repeat)";
+          };
+        };
+        "<leader><leader>" = {
+          mode = "n";
+          action = "buffers";
+          options = {
+            desc = "[ ] Find existing buffers";
+          };
+        };
+      };
+      settings = {
+        extensions.__raw = "{ ['ui-select'] = { require('telescope.themes').get_dropdown() } }";
+      };
     };
   };
-
+  # https://nix-community.github.io/nixvim/keymaps/index.html
   keymaps = [
+    # Slightly advanced example of overriding default behavior and theme
     {
-      key = "<leader>sh";
       mode = "n";
-      action = "help_tags";
+      key = "<leader>/";
+      # You can pass additional configuration to Telescope to change the theme, layout, etc.
+      action.__raw = ''
+        function()
+          require('telescope.builtin').current_buffer_fuzzy_find(
+            require('telescope.themes').get_dropdown {
+              winblend = 10,
+              previewer = false
+            }
+          )
+        end
+      '';
       options = {
-        desc = "[S]earch [H]elp";
+        desc = "[/] Fuzzily search in current buffer";
       };
     }
     {
-      key = "<leader>sk";
       mode = "n";
-      action = "keymaps";
+      key = "<leader>s/";
+      # It's also possible to pass additional configuration options.
+      #  See `:help telescope.builtin.live_grep()` for information about particular keys
+      action.__raw = ''
+        function()
+          require('telescope.builtin').live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files'
+          }
+        end
+      '';
       options = {
-        desc = "[S]earch [K]eymaps";
+        desc = "[S]earch [/] in Open Files";
       };
     }
+    # Shortcut for searching your Neovim configuration files
     {
-      key = "<leader>sf";
       mode = "n";
-      action = "find_files";
+      key = "<leader>sn";
+      action.__raw = ''
+        function()
+          require('telescope.builtin').find_files {
+            cwd = vim.fn.stdpath 'config'
+          }
+        end
+      '';
       options = {
-        desc = "[S]earch [F]iles";
-      };
-    }
-    {
-      key = "<leader>ss";
-      mode = "n";
-      action = "builtin";
-      options = {
-        desc = "[S]earch [S]elect Telescope";
-      };
-    }
-    {
-      key = "<leader>sw";
-      mode = "n";
-      action = "grep_string";
-      options = {
-        desc = "[S]earch current [W]ord";
-      };
-    }
-    {
-      key = "<leader>sg";
-      mode = "n";
-      action = "live_grep";
-      options = {
-        desc = "[S]earch by [G]rep";
-      };
-    }
-    {
-      key = "<leader>sd";
-      mode = "n";
-      action = "diagnostics";
-      options = {
-        desc = "[S]earch [D]iagnostics";
-      };
-    }
-    {
-      key = "<leader>sr";
-      mode = "n";
-      action = "resume";
-      options = {
-        desc = "[S]earch [R]esume";
-      };
-    }
-    {
-      key = "<leader>s";
-      mode = "n";
-      action = "oldfiles";
-      options = {
-        desc = "[S]earch Recent Files ('.' for repeat)";
-      };
-    }
-    {
-      key = "<leader><leader>";
-      mode = "n";
-      action = "buffers";
-      options = {
-        desc = "[ ] Find existing buffers";
+        desc = "[S]earch [N]eovim files";
       };
     }
   ];
