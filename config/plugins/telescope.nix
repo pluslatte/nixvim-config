82f751c5c4f2799e8310f4d0ea4ace0c8206c54a
pluslatte <debrisfield0201@gmail.com>
2024-12-11 20:14:50 +0900

    oops

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -10,75 +10,86 @@
     };
   };
 
-  keymaps = {
-    "<leader>sh" = {
+  keymaps = [
+    {
+      key = "<leader>sh";
       mode = "n";
       action = "help_tags";
       options = {
         desc = "[S]earch [H]elp";
       };
-    };
-    "<leader>sk" = {
+    }
+    {
+      key = "<leader>sk";
       mode = "n";
       action = "keymaps";
       options = {
         desc = "[S]earch [K]eymaps";
       };
-    };
-    "<leader>sf" = {
+    }
+    {
+      key = "<leader>sf";
       mode = "n";
       action = "find_files";
       options = {
         desc = "[S]earch [F]iles";
       };
-    };
-    "<leader>ss" = {
+    }
+    {
+      key = "<leader>ss";
       mode = "n";
       action = "builtin";
       options = {
         desc = "[S]earch [S]elect Telescope";
       };
-    };
-    "<leader>sw" = {
+    }
+    {
+      key = "<leader>sw";
       mode = "n";
       action = "grep_string";
       options = {
         desc = "[S]earch current [W]ord";
       };
-    };
-    "<leader>sg" = {
+    }
+    {
+      key = "<leader>sg";
       mode = "n";
       action = "live_grep";
       options = {
         desc = "[S]earch by [G]rep";
       };
-    };
-    "<leader>sd" = {
+    }
+    {
+      key = "<leader>sd";
       mode = "n";
       action = "diagnostics";
       options = {
         desc = "[S]earch [D]iagnostics";
       };
-    };
-    "<leader>sr" = {
+    }
+    {
+      key = "<leader>sr";
       mode = "n";
       action = "resume";
       options = {
         desc = "[S]earch [R]esume";
       };
-    };
-    "<leader>s" = {
+    }
+    {
+      key = "<leader>s";
       mode = "n";
       action = "oldfiles";
       options = {
         desc = "[S]earch Recent Files ('.' for repeat)";
       };
-    };
-    "<leader><leader>" = {
+    }
+    {
+      key = "<leader><leader>";
       mode = "n";
       action = "buffers";
       options = {
         desc = "[ ] Find existing buffers";
-    };
-  };
+      };
+    }
+  ];
 }
