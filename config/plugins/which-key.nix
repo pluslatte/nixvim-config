d03e9dd4e5375cc3e9fab683b587b4e0bbbb0848
pluslatte <debrisfield0201@gmail.com>
2024-12-11 21:33:05 +0900

    which-key keymap group

--- config/plugins/which-key.nix
+++ config/plugins/which-key.nix
@@ -2,6 +2,15 @@
   plugins = {
     which-key = {
       enable = true;
+      
+      settings = {
+        spec = [
+          {
+            __unkeyed-1 = "<Leader>b";
+            group = "Buffers";
+          }
+        ];
+      };
     };
   };
 }
