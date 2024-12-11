fe7734c5c1e712be35ffe64dcf54b682ed7ab891
pluslatte <debrisfield0201@gmail.com>
2024-12-11 18:59:32 +0900

    oops

--- config/plugins/neo-tree.nix
+++ config/plugins/neo-tree.nix
@@ -2,17 +2,17 @@
   plugins = {
     neo-tree = {
       enable = true;
-
-      keymaps = [
-        {
-          key = "<Leader>e";
-          action = "<cmd>Neotree toggle<cr>";
-          options = {
-            desc = "Toggle NeoTree";
-          };
-        }
-      ];
     };
     web-devicons.enable = true;
   };
+
+  keymaps = [
+    {
+      key = "<Leader>e";
+      action = "<cmd>Neotree toggle<cr>";
+      options = {
+        desc = "Toggle NeoTree";
+      };
+    }
+  ];
 }
