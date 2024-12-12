babd769f77c39e7598c6a6ca983925111f89ff88
pluslatte <debrisfield0201@gmail.com>
2024-12-12 17:56:56 +0900

    fix which-key desc was too long

--- config/plugins/which-key.nix
+++ config/plugins/which-key.nix
@@ -2,12 +2,16 @@
   plugins = {
     which-key = {
       enable = true;
-      
+
       settings = {
         spec = [
           {
             __unkeyed-1 = "<Leader>b";
             group = "Buffers";
+          }
+          {
+            __unkeyed-2 = "gx";
+            desc = "Opens filepath or URI";
           }
         ];
       };
