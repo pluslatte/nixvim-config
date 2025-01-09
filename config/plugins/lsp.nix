04c4fa5d5a5472c319d41b398183809c7f8a7acc
pluslatte <debrisfield0201@gmail.com>
2025-01-09 14:21:51 +0900

    workaround for rust-analyzer issue

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,4 +1,18 @@
 {
+  # Workaround
+  # https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+  extraConfigLua = ''
+    for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+            if err ~= nil and err.code == -32802 then
+                return
+            end
+            return default_diagnostic_handler(err, result, context, config)
+        end
+    end
+  '';
+
   plugins = {
     cmp-nvim-lsp.enable = true;
     # fidget.enable = true;
