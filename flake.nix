a9c9e695f34a95451a505f1f30882da9c3f2fe6e
pluslatte <debrisfield0201@gmail.com>
2026-06-17 10:09:41 +0900

    version pin

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixvim.url = "github:nix-community/nixvim/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
